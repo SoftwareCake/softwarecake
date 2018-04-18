ce186ebb7b3a7ee5900913b10c9ee851e4042e84
Software Cake <36171157+SoftwareCake@users.noreply.github.com>
2018-04-18 12:18:17 -0400

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Come on, you have a name.",
+                    minlength: "Your name must consist of at least 2 characters"
+                },
+                website: {
+                    required: "You have an email, don't you?",
+                    minlength: "Your address must consist of at least 4 characters"
+                },
+                email: {
+                    required: "No email, No message"
+                },
+                message: {
+                    required: "um...yea, you have to write something to send this form.",
+                    minlength: "Is... Is that it?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 0.15, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 0.15, function() {
+                            $('#error').fadeIn()
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
